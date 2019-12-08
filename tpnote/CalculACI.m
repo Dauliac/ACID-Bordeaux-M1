604f4409ff32094b27e232a220aae5bc2190d4b5
asatger <42678516+asatger@users.noreply.github.com>
2019-12-08 17:48:59 +0100

    Add ACI

--- tpnote/CalculACI.m
+++ tpnote/CalculACI.m
@@ -18,3 +18,14 @@ function [ V ] = CalculACI( C1, C2 )
     invSwSb = invSw * Sb;
     [V, d] = eig(invSwSb)
 end
+
+function [ res ] = calculACI( train1,train2 )
+%CALCULACI Summary of this function goes here
+%   Detailed explanation goes here
+
+    C1=cov(train1);
+    C2=cov(train2);
+    res=((C1+C2).^-1)*transpose(mean(train1)-mean(train2));
+
+
+end
